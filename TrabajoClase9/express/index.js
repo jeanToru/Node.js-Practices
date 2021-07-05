4ced6000fafc0af65ec47bbed07b1ffeb4983f17
Jean Brown <jeantoruno00@gmail.com>
2021-07-04 23:11:05 -0700

    Trabajo en clase 9

---
+++ TrabajoClase9/express/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    type: 'application/x-www-form-urlencoded',
+    extended: true,
+}))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/about.html'));
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('Hola mundo...');
+})
+
+app.listen(3501)
