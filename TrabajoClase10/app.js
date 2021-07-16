8333d771e2826ae1d6a635ac9d007ea41cce5c03
Jean Brown <jeantoruno00@gmail.com>
2021-07-16 13:50:40 -0700

    Trabajo en clase

--- TrabajoClase10/app.js
+++ TrabajoClase10/app.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const app = express();
 require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoute = require('./routes/user.route');
+const app = express();
 
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, HOSTNAME, ()=>{
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.log(error));
+db.once('open', () => console.log('connection to db established'));
+
+app.use(express.json());
+app.use(express.urlencoded({
+  type:'app;ication/x-www-form-urlencoded',
+  extended: true  
+}))
+
+app.use('/', userRoute);
+
+app.use('*', (req, res) => {
+    res.status(404)
+    res.send('Path cannot found')
+});
+
+app.listen(PORT, HOSTNAME, () => {
     console.log(`Server runnig on ${HOSTNAME}:${PORT}`);
 })
